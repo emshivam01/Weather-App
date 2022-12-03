930f5b11ade9269d07bcba71cc0795c53c434e80
Shivam Sharma <emshivam01@gmail.com>
2022-12-03 21:37:42 +0530

    fixed some issues in logic

--- script.js
+++ script.js
@@ -1,47 +1,94 @@
-let input = document.getElementById("cityName");
-let apikey = "7011fc130cfa11c2a154982db812bace";
 let btn = document.getElementById("btn");
-let temp = document.getElementById("temp");
-let currLocation = document.getElementById("location");
-let humidity = document.getElementById("humidity");
-let feels = document.getElementById("feels_like");
-let weatherImg = document.getElementById("weather-img");
-let weatherStatus = document.getElementById("weather-status");
+let input = document.getElementById("input");
+let div = document.getElementById("division");
+let section = document.createElement("section");
+let tempSec = document.createElement("div");
+let humidSec = document.createElement("div");
+let feelsLikeSec = document.createElement("div");
+let temp = document.createElement("p");
+let weatherType = document.createElement("p");
+let place = document.createElement("p");
+let humidity = document.createElement("p");
+let humidityText = document.createElement("p");
+let feels_like = document.createElement("p");
+let feels_likeText = document.createElement("p");
+let otherDesc = document.createElement("div");
+
+let apikey = "7011fc130cfa11c2a154982db812bace"; // API KEY
 
 btn.addEventListener("click", () => {
-  console.log(input.value);
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apikey}&units=metric`
   )
     .then((res) => res.json())
     .then((res) => {
+      section.innerHTML = "";
+      input.value = "";
+
+      section.classList.add(
+        "shadow-lg",
+        "p-2",
+        "py-6",
+        "rounded-md",
+        "flex",
+        "flex-col",
+        "justify-center",
+        "items-center"
+      );
+
+      temp.classList.add("text-5xl", "font-normal");
+
+      weatherType.classList.add(
+        "text-center",
+        "text-xl",
+        "font-medium",
+        "mt-4"
+      );
+
+      place.classList.add("mt-4", "text-center", "text-xl", "font-medium");
+
+      otherDesc.classList.add(
+        "flex",
+        "justify-between",
+        "items-center",
+        "w-full",
+        "px-2",
+        "mt-6"
+      );
+
+      humidSec.classList.add("flex");
+      feelsLikeSec.classList.add("flex");
+
+      place.innerText = `${res.name}`;
+      humidity.innerText = ` ${res.main.humidity} %`;
+      feels_like.innerHTML = ` ${Math.round(res.main.feels_like)} &#176; C`;
+      humidityText.innerHTML = "Humidity: &nbsp;";
+      feels_likeText.innerHTML = "Feels Like: &nbsp;";
+      temp.innerHTML = `${Math.round(res.main.temp)} &#176; C`;
+      weatherType.innerText = `${res.weather[0].main}`;
+      tempSec.appendChild(temp);
+      tempSec.appendChild(weatherType);
+
+      // Humidity Section
+
+      humidSec.appendChild(humidityText);
+      humidSec.appendChild(humidity);
+
+      // Feels Like Seciton
+
+      feelsLikeSec.appendChild(feels_likeText);
+      feelsLikeSec.appendChild(feels_like);
+
+      otherDesc.appendChild(humidSec);
+      otherDesc.appendChild(feelsLikeSec);
+
+      // Appending Inside Section
+      section.appendChild(tempSec);
+      section.appendChild(place);
+      section.appendChild(otherDesc);
+
+      // Appending Inside Main Division
+      div.insertBefore(section, input);
       console.log(res);
-      temp.innerText = Math.round(res.main.temp);
-      currLocation.innerText = res.name;
-      humidity.innerText = res.main.humidity;
-      feels.innerText = Math.round(res.main.feels_like);
-      weatherStatus.innerText = res.weather[0].main;
-
-      console.log(res.weather[0].main);
-      console.log(typeof res.weather[0].main);
-
-      if (res.weather[0].main == "Clouds") {
-        weatherImg.src = "./assets/New Icons/cloud.png";
-      }
-      else if (res.weather[0].main == "Clear" || "Sunny") {
-        weatherImg.src = "./assets/New Icons/sunny.png";
-      }
-      else if (res.weather[0].main == "Rainy") {
-        weatherImg.src = "./assets/New Icons/rainy.png";
-      }
-      else if (res.weather[0].main == "Haze" || "Smoke") {
-        weatherImg.src = "./assets/New Icons/haze.png";
-      }
-      else if (res.weather[0].main == "Snow") {
-        weatherImg.src = "./assets/New Icons/snowy.png";
-      }
-      else if (res.weather[0].main == "storm") {
-        weatherImg.src = "./assets/New Icons/storm.png";
-      }
     });
 });
